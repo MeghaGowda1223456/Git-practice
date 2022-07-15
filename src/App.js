1bb03a81b425d8595dc0823ad079b0f3257e597a
devinachakraborty <devinachakraborty@gmail.com>
2022-07-15 16:20:42 +0530

    Devina's changes in Uma's Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from "./logo.svg";
 
 import "./App.css";
 import DataComponents from "./components/DataComponents";
-<<<<<<< HEAD
-import { useState } from "react";
-=======
 import  { useState } from "react";
->>>>>>> 5fe3aee5635fe39f6f78c216d7dfb912a7c84d1f
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
 import Umashankar from "./components/Umashankar";
@@ -14,6 +10,8 @@ import Lolakshi from "./components/Lolakshi";
 import Ramya from './components/Ramya';
 import Devina from "./components/Devina";
 import Jagath from './components/Jagath';
+import Fruits from './components/Shubham';
+import Vijay from './components/vijay';
 
 function App() {
   const [state, setState] = useState(true);
