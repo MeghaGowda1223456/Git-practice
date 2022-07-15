5cfc8e39801e581120e8d4b53a4ca1b4608a3322
lms project <umashankarjabagond@gmail.com>
2022-07-15 15:39:55 +0530

    Umashankar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Umashankar from "./components/Umashankar";
 
 function App() {
   const [state, setState] = useState(true);
   return (
     <div className="App">
+      <Umashankar/>
       <ComponentB/>
       {state ? <DataComponents setState={setState} /> : <ComponentA />}
     </div>
