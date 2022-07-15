c0857ab309d01164d08d8f9ef031a9aaedc036ac
Pratish-Gavit <pratish008@gmail.com>
2022-07-15 15:49:24 +0530

    Changes made in shubham component

--- src/components/Shubham.js
+++ src/components/Shubham.js
@@ -1,4 +1,14 @@
-let a=20;
-let b=20;
-let sum=a+b;
-console.log(sum);
+// let a=20;
+// let b=20;
+// let sum=a+b;
+// console.log(sum);
+
+import React from 'react'
+
+function Shubham() {
+  return (
+    <div>Changes done by pratish</div>
+  )
+}
+
+export default Shubham
