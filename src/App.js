fee8b44bc9af62f054565edb235763a71c17b9e4
Ramyashree-SR <ramyashree4856@gmail.com>
2022-07-15 15:27:39 +0530

    Ramya

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Ramya from './components/Ramya';
 
 function App() {
   const [state, setState] = useState(true);
@@ -11,8 +12,12 @@ function App() {
     <div className="App">
       <ComponentB/>
       {state ? <DataComponents setState={setState} /> : <ComponentA />}
+
+      <Ramya/>
     </div>
   );
 }
+
+
 
 export default App;
