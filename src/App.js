1891cca4ca4cb8b16443a4b5e7269590e025085b
Ramyashree-SR <ramyashree4856@gmail.com>
2022-07-15 16:27:35 +0530

    Ramya updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import React from 'react'
 import "./App.css";
 import DataComponents from "./components/DataComponents";
 import  { useState } from "react";
@@ -10,7 +11,7 @@ import Ramya from './components/Ramya';
 import Devina from "./components/Devina";
 import Jagath from './components/Jagath';
 import Fruits from "./components/Shubham";
-import Fruits from './components/Shubham';
+
 import Vijay from './components/vijay';
 
 function App() {
