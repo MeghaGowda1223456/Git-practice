ab31e09a74819246d830593409daec25d71120a4
unknown <lolakshi444@gmail.com>
2022-07-15 15:33:37 +0530

    Update from Lolakshi

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import DataComponents from "./components/DataComponents";
 import { useState } from "react";
 import ComponentA from "./components/ComponentA";
 import ComponentB from "./components/ComponentB";
+import Lolakshi from "./components/Lolakshi";
 
 function App() {
   const [state, setState] = useState(true);
   return (
     <div className="App">
       <ComponentB/>
+      <Lolakshi/>
       {state ? <DataComponents setState={setState} /> : <ComponentA />}
     </div>
   );
