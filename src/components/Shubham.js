0808e1f83ea95cd7d91884c52eab0e584c0d852c
shubham74817 <shubham74817@gmail.com>
2022-07-15 16:27:41 +0530

    Shubham updated

--- src/components/Shubham.js
+++ src/components/Shubham.js
@@ -4,17 +4,14 @@ function Fruits(){
     return(
         <div>
             This is fruits section.
-<<<<<<< HEAD
             Different kind of fruits are available here.🌂☂️☔🌈
             🍇🍈🍉🍊🍋🍌🍐🍏🍎🥭🍍
-          </div>
-=======
+         
             Different kind of fruits are available here.
             <h1>Hello Subham</h1>
 
             Code  Added
         </div>
->>>>>>> b5b8fa52c3e265e0ca5516094821fbf626fad056
     )
 }
 export default Fruits;
